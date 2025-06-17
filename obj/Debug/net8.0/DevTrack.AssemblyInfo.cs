db7b161f3f7665c4d40486608f17edd1788a2064
brandon <141857607+bam2424@users.noreply.github.com>
2025-06-17 11:58:07 -0400

    Use nixpacks.toml for .NET 8.0 configuration

--- obj/Debug/net8.0/DevTrack.AssemblyInfo.cs
+++ obj/Debug/net8.0/DevTrack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e5a523ce0feac586f5e506a2ede35bf3676b54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c94b18939a4545a72d716b1a854ba7493230f1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
