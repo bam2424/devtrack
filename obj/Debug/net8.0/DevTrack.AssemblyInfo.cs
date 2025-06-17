d7d97abe991fa10a48d9b84ac5e89350e04fc3c7
brandon <141857607+bam2424@users.noreply.github.com>
2025-06-17 14:31:01 -0400

    Enhance authentication pages styling: modern dark theme with improved UX for login, register, and external login pages

--- obj/Debug/net8.0/DevTrack.AssemblyInfo.cs
+++ obj/Debug/net8.0/DevTrack.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb2939a2554ad1ef8b68e8f48072e462f53a2866")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad87f50339d8a859635d38baef688e6330b52443")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
